e07a96f6b74125d7218211634e94d0d222f7d1e8
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-07-16 21:51:31 +0900

    スペースを追加

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="content">
-      <Swiper
-        :slides-per-view="1"
-        :loop="true"
-        :autoplay="{ delay: 3000 }"
-        :modules="[SwiperAutoplay]"
-      >
+        <Swiper
+            :slides-per-view="1"
+            :loop="true"
+            :autoplay="{ delay: 3000 }"
+            :modules="[SwiperAutoplay]"
+        >
         <SwiperSlide><p>1</p></SwiperSlide>
         <SwiperSlide><p>2</p></SwiperSlide>
         <SwiperSlide><p>3</p></SwiperSlide>
