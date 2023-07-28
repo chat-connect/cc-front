658bb4b3c03b62acf8b4df9042591ddbf134ba3c
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-07-28 18:32:52 +0900

    ルーム作成ページを仮置き

---
+++ components/Room/Create.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <v-card flat style="margin: 0px 10px 0px 10px;">
+            <v-card-item>
+                <div style="height: 550px;">
+                    {{ "NewRoom" }}
+                </div>
+            </v-card-item>
+        </v-card>
+    </div>
+</template>
---
+++ pages/room/createRoom.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <v-container class="main_container">
+            <v-row align-content="center">
+                <v-col align="center">
+                    <Create />
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.main_container {
+    position: relative;
+    top: 64px;
+}
+</style>
