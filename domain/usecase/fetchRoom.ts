decabcc599b2f9a5a755b024a5b4086be7f777e7
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-08-09 16:50:42 +0900

    ルーム作成処理を追加

--- domain/usecase/fetchRoom.ts
+++ domain/usecase/fetchRoom.ts
@@ -1,4 +1,5 @@
 import { RoomList } from "@/domain/entity/room/roomList"
+import { RoomCreate } from "@/domain/entity/room/roomCreate"
 import { ApiClient } from "@/infra/api/apiClient"
 
 export class FetchRoom {
@@ -13,6 +14,14 @@ export class FetchRoom {
         const access_token = useCookie('access_token')
         const response: RoomList = await this.apiClient.get("/api/room/" + userKey + "/room_list", access_token.value)
         
+        return response
+    }
+
+    // roomCreate ルーム作成
+    async roomCreate(body: any, userKey: string): RoomCreate {
+        const access_token = useCookie('access_token')
+        const response: RoomCreate = await this.apiClient.post("/api/room/" + userKey + "/room_create", body, access_token.value)
+
         return response
     }
 }
